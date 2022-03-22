552bce069eb55a4df49ba6a479c92764feb6a573
lasit <lasitmittal@gmail.com>
2022-03-22 19:55:14 +0530

    Project

---
+++ crmapp/src/main/java/com/greatlearning/crmapp/service/CustomerService.java
@@ -0,0 +1,18 @@
+package com.greatlearning.crmapp.service;
+
+import java.util.List;
+
+import com.greatlearning.crmapp.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> findAll();
+	
+	public Customer findById(int theId);
+	
+	public void save(Customer theCustomer);
+	
+	public void deleteById(int theId);
+	
+	
+}
